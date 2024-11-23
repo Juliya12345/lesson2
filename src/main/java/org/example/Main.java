a9cb7cf414dbab54b1eed11c0862f6eb0ef400ee
Juliya12345 <150064686+Juliya12345@users.noreply.github.com>
2024-11-23 22:36:40 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("lesson212345");
+        System.out.println("lesson212345" + "conflict!!!");
     }
 }
